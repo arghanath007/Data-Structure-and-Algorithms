8f871aa7fcbba3972ff21a96055beaa21a1ea8ba
Argha Nath <54589605+arghanath007@users.noreply.github.com>
2022-03-09 13:59:03 +0530

    Networking and Cloud Computing

---
+++ Pseudo Code Test/accenture_1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;   
+int main()
+{
+    int i;
+    i = 3;
+
+    do{
+        cout<<i+3;
+        i = i - 1;
+    }   
+    while(i!=0);
+    return 0;
+}
+
